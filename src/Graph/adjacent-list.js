9bf4bcf6ffe0b69e3bb37f5ab56bc154f2fd2cad
arthur <arthur.oceanus@gmail.com>
2024-06-29 15:09:19 +0800

    add create and remove methods in graph

--- src/Graph/adjacent-list.js
+++ src/Graph/adjacent-list.js
@@ -8,150 +8,60 @@
  * 缺點：除非在同一個 bucket 內，否則資料（Node）之間不會彼此參照。
  */
 
-// adjacent list
-// TODO: Implement Graph class
+// Adjacent list
 class Graph {
-  /** @param {number} size */
-  constructor(size) {
-    this.data = new Array(size);
-    this.length = this.data.length;
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  /** @param {number} vertex */
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
 
   /**
-   * @param {string} key
-   * @description hash func
+   * @param {number} source @param {number} destination **/
+  addEdge(source, destination) {
+    if (!this.adjacencyList[source]) this.addVertex(source);
+    if (!this.adjacencyList[destination]) this.addVertex(destination);
+    this.adjacencyList[source].push(destination);
+    this.adjacencyList[destination].push(source);
+  }
+
+  /**
+   * @param {number} source
+   * @param {number} destination
    **/
-  #hash(key) {
-    let hash = 0;
-    for (let i = 0; i < key.length; i++) {
-      hash = (hash + key.charCodeAt(i) * i) % this.length;
+  removeEdge(source, destination) {
+    if (this.adjacencyList[source]) {
+      this.adjacencyList[source] = this.adjacencyList[source].filter(
+        /** @param {number} adjacentVertex */
+        (adjacentVertex) => adjacentVertex !== destination,
+      );
     }
-    return hash;
-  }
-
-  /**
-   * @param {string} key
-   * @description get bucket by key
-   **/
-  #getBucket(key) {
-    const idx = this.#hash(key); // 算出要資料保存的位址(idx)
-    const bucket = this.data[idx]; // 取出 bucket
-    return bucket;
-  }
-
-  /**
-   * @param {string} key
-   * @param {array} bucket
-   * @description find index
-   **/
-  #findIndex(key, bucket) {
-    // // 如果沒有 collision 則是 O(1)，否則會是 O(n)
-    const endIdx = bucket.length;
-    let currentIdx = -1;
-    while (currentIdx++ < endIdx) {
-      const [_key] = bucket[currentIdx];
-      if (_key === key) return currentIdx;
+    if (this.adjacencyList[destination]) {
+      this.adjacencyList[destination] = this.adjacencyList[destination].filter(
+        /** @param {number} adjacentVertex */
+        (adjacentVertex) => adjacentVertex !== source,
+      );
     }
-    return -1;
-    // return bucket.findIndex(([_key])=> _key === key);
   }
 
-  /**
-   * @param {string} key
-   * @param {any} value
-   * @description set key, value
-   **/
-  set(key, value) {
-    const idx = this.#hash(key); // 算出要儲存的位址(idx)
-    const bucket = this.data[idx]; // 取出要保存的 bucket
-
-    // 如果 bucket 不存在，初始化陣列並存入第一筆資料
-    if (!bucket) {
-      this.data[idx] = [[key, value]];
-      return this;
+  /** @param {number} vertex */
+  removeVertex(vertex) {
+    // Remove all of edge of vertex that store in other vertices
+    while (this.adjacencyList[vertex]) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
     }
-
-    this.data[idx].push([key, value]);
-    return this;
+    // Remove the vertex itself
+    delete this.adjacencyList[vertex];
   }
 
-  /**
-   * @param {string} key
-   * @description get value by key
-   **/
-  get(key) {
-    const bucket = this.#getBucket(key);
-    if (!bucket) return;
-    const index = this.#findIndex(key, bucket);
-    if (index === -1) return;
-    return bucket[index][1]; // return value
-  }
+  // printGraph()
 
-  /**
-   * @param {string} key
-   * @param {any} newValue
-   * @description update value by key
-   **/
-  update(key, newValue) {
-    const bucket = this.#getBucket(key);
-    if (!bucket) return;
-    const index = this.#findIndex(key, bucket);
-    if (index === -1) return;
-    bucket[index][1] = newValue;
-    return bucket[index];
-  }
-
-  /**
-   * @param {string} key
-   * @description delete key
-   **/
-  delete(key) {
-    const bucket = this.#getBucket(key);
-    if (!bucket) return;
-    const index = this.#findIndex(key, bucket);
-    if (index === -1) return;
-    bucket.splice(index, 1);
-  }
-
-  /**
-   * @description get all keys
-   **/
-  // 取得所有 key，O(n)
-  keys() {
-    return this.data.flatMap((v) =>
-      v ? v.map(/** @param {array} v*/ (v) => v[0]) : [],
-    );
-
-    // let keys = [];
-    // for (let i = 0; i < this.length; i++) {
-    //   const bucket = this.data[i];
-    //   if (bucket) {
-    //     for (let j = 0; j < bucket.length; j++) {
-    //       const [key] = bucket[j];
-    //       keys.push(key);
-    //     }
-    //   }
-    // }
-    // return keys;
-  }
-
-  /**
-   * @description get all values
-   **/
-  // 取得所有 value，O(n)
-  values() {
-    return this.data.flatMap((v) =>
-      v ? v.map(/** @param {array} v*/ (v) => v[1]) : [],
-    );
-  }
-
-  /**
-   * @description get all [key,value][]
-   **/
-  // 取得所有 key, value，O(n)
-  entries() {
-    return this.data.flatMap((v) => (v ? v : []));
-  }
+  // bfs(v)
+  // dfs(v)
 }
 
 // const IGraph = new Graph(3);
